7fb61215ab1a8d410147e542e51c229b87376667
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2026-05-21 10:21:05 -0300

    js

---
+++ script.js
@@ -0,0 +1,9 @@
+let musica = document.querySelector('audio');
+
+document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
+function tocarMusica() {
+    musica.play();
+    document.querySelector('botao-pause').style.display = 'block';
+
+}
