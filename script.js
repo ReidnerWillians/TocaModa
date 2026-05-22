ea15e92eeebaa6a4f31cd86b814d7c7a4bfe73c3
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2026-05-22 10:28:22 -0300

    js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let musica = document.querySelector('audio');
 
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
-document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
-
 function tocarMusica() {
     musica.play();
-    document.querySelector('.botao-play').style.display = 'none';
     document.querySelector('.botao-pause').style.display = 'block';
+    document.querySelector('.botao-play').style.display = 'none';    
 }
+
+document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
 function pausarMusica() {
     musica.pause();
-    document.querySelector('.botao-play').style.display = 'block';}
+    document.querySelector('.botao-play').style.display = 'block';
     document.querySelector('.botao-pause').style.display = 'none';
-    
+}  
 
 function atualizarBarra() {
     let barra = document.querySelector('progress');
