c412aeb1433e7f4f1ec569411e0d13e63d5381b5
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2026-05-22 17:22:36 -0300

    js

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 let musica = document.querySelector('audio');
 
+// Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
 
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
 
+musica.addEventListener('timeupdate', atualizarBarra);
+
+//Funções
+/*
+function renderizarMusica(index){
+    musica.setAttribute('src', musicas[index].src);
+    musica.addEventListener('loadeddata', () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        imagem.src = musicas[index].img;
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}*/
+
 function tocarMusica(){
     musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
-    document.querySelector('.botao-play').style.display = 'none';    
+    document.querySelector('.botao-play').style.display = 'none';
 }
 
 function pausarMusica(){
     musica.pause();
-    document.querySelector('.botao-play').style.display = 'block';
     document.querySelector('.botao-pause').style.display = 'none';
-}  
+    document.querySelector('.botao-play').style.display = 'block';
+}
 
 function atualizarBarra(){
     let barra = document.querySelector('progress');
     barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%';
     let tempoDecorrido = document.querySelector('.inicio');
-    tempoDecorrido.textContent = Math.floor(musica.currentTime);
+    tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
 }
 
+/*function segundosParaMinutos(segundos){
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10){
+        campoSegundos = '0' + campoSegundos;
+    }
+
+    return campoMinutos+':'+campoSegundos;
+}*/
